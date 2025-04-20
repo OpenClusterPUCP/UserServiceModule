83c644c8d4d819ebb9e4c56625c167c43100124e
Hineill <hineill.cespedes@pucp.edu.pe>
2025-04-20 00:49:52 -0500

    first commit

---
+++ src/main/java/com/example/userservicemodule/UserServiceModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.userservicemodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/userservicemodule/UserServiceModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userservicemodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
